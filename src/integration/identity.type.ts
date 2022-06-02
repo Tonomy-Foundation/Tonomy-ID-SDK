32955079e06015f8725a00da6eb8027eae68582f
Jack Tanner <jack@jackandtheblockstalk.com>
2022-06-02 15:41:49 +0200

    Added identity types

---
+++ src/integration/identity.type.ts
@@ -0,0 +1,35 @@
+
+type LoginOptions = {
+    createKey?: boolean // Create a new key to use for smart contracts (on-chain) or authorizations (off-chain)
+    onChainAuthorization?: boolean // True will add the public key of the created private key to the on-chain account to be used in smart contracts.
+}
+
+type LoginResponseCredentialSubject = {
+    id: did
+    jwk?: any // public key confirming the key that was created, if requested
+    tx?: string // The transaction ID used to authorize the key on chain, if requested
+}
+
+type LogoutResponseCredentialSubject = {
+    id: did
+    tx?: string // The transaction ID used to deauthorize the key on chain, if requested
+}
+
+interface Identity {
+    login(options?: LoginOptions): Promise<VerifiableCredential<LoginResponseCredentialSubject>>
+    logout(): Promise<VerifiableCredential<LogoutResponseCredentialSubject>>
+}
+
+interface Transactions {
+    signTrx(): any
+}
+
+interface Credentials {
+    create(): any
+    share(): any
+    store(): any
+}
+
+interface Communictaion {
+    send(): any
+}
