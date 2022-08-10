6411b615b4cfb1cff5d91b8aee141d8bab8b0c71
Jack Tanner <jack@jackandtheblockstalk.com>
2022-08-10 11:26:46 +0200

    Changed Authenticator to use option parameter

--- src/authenticator.ts
+++ src/authenticator.ts
@@ -2,51 +2,74 @@ import { PrivateKey, PublicKey } from '@greymass/eosio';
 
 enum AuthenticatorLevel { Password, PIN, Fingerprint, Local };
 
-interface Authenticator {
-    // TODO: turn each of the inputs into objects...
+/**
+ * @param level - The security level of the key
+ * @param privateKey - The private key to be stored
+ * @param [challenge] - A challenge that needs to be presented in order for the key to be used
+ */
+type StoreKeyOptions = {
+    level: AuthenticatorLevel;
+    privateKey: PrivateKey;
+    challenge?: string;
+}
 
+/**
+ * @param level - The security level of the key
+ * @param data - The data that will be used to create a digital signature
+ * @param [challenge] - A challenge that needs to be presented in order for the key to be used
+ */
+type SignDataOptions = {
+    level: AuthenticatorLevel;
+    data: string | Uint8Array;
+    challenge?: string
+}
+
+/**
+ * @param level - The security level of the key
+ */
+type GetKeyOptions = {
+    level: AuthenticatorLevel;
+}
+
+interface Authenticator {
     /**
      * Stores a private key that can be used later for signing.
      *
      * @remarks
      * Once a private key is stored, it may no longer be accessible.
      *
-     * @param level - The security level of the key
-     * @param privateKey - The private key to be stored
-     * @param [challenge] - A challenge that needs to be presented in order for the key to be used
+     * @param options - Options for storing the key
      * @returns The PublicKey
      */
-    storeKey(level: AuthenticatorLevel, privateKey: PrivateKey, challenge?: string): PublicKey
+    storeKey(options: StoreKeyOptions): PublicKey
 
     /**
      * Signs the hash of data with a stored private key
      *
-     * @param level - The security level of the key
-     * @param data - The data that will be used to create a digital signature
-     * @param [challenge] - A challenge that needs to be presented in order for the key to be used
+     * @param options - Options for signing data
      * @returns A digital signature of the SHA256 hashed data
      */
-    signData(level: AuthenticatorLevel, data: string | Uint8Array, challenge?: string): string
+    signData(options: SignDataOptions): string
 
     /**
      * Returns the public key of a stored private key
      *
-     * @param level - The security level of the key
+     * @param options - Options for retreiving the key
      * @returns The PublicKey
      */
-    getKey(level: AuthenticatorLevel): PublicKey
+    getKey(options: GetKeyOptions): PublicKey
 }
 
 class JsAuthenticator implements Authenticator {
-    storeKey(level: AuthenticatorLevel, privateKey: PrivateKey, challenge: string): PublicKey {
+    storeKey(options: StoreKeyOptions): PublicKey {
         return PublicKey.from("");
     }
 
-    signData(level: AuthenticatorLevel, data: string | Uint8Array, challenge?: string): string {
+    signData(options: SignDataOptions): string {
         return "";
     }
 
-    getKey(level: AuthenticatorLevel): PublicKey {
+    getKey(options: GetKeyOptions): PublicKey {
         return PublicKey.from("");
     }
 }
