f9701d9244a0b07bf7436d6effd3e4ee23224426
Jack Tanner <jack@jackandtheblockstalk.com>
2023-02-05 23:47:31 +0100

    Run test.sh

---
+++ integration-test/run-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -u ## exit if you try to use an uninitialised variable
+set -e ## exit if any statement fails
+
+# Make sure working dir is same as this dir, so that script can be excuted from another working directory
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "${PARENT_PATH}/.."
+pwd
+
+# Setup Tonomy Contracts
+if [ ! -d "Tonomy-Contracts" ]; then
+    git clone https://github.com/Tonomy-Foundation/Tonomy-Contracts.git
+fi
+cd Tonomy-Contracts
+git checkout feature/96-integration-tests-in-sdk
+git pull
+
+# Build Tonomy Contracts
+./build-contracts.sh
+
+# Create docker image
+docker build . -f ./blockchain/Dockerfile --force-rm -t eosiobuild
+
