08c4327dba47e67aafaf9f9b1f49a1ed5b04244e
Jack Tanner <jack@jackandtheblockstalk.com>
2023-02-06 00:08:19 +0100

    Running integration tests

--- integration-test/build-and-run-tests.sh
+++ integration-test/build-and-run-tests.sh
@@ -6,13 +6,13 @@ set -e ## exit if any statement fails
 # Make sure working dir is same as this dir, so that script can be excuted from another working directory
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${PARENT_PATH}/.."
-pwd
 
 # Setup Tonomy Contracts
 if [ ! -d "Tonomy-Contracts" ]; then
     git clone https://github.com/Tonomy-Foundation/Tonomy-Contracts.git
 fi
 cd Tonomy-Contracts
+# TODO update to development
 git checkout feature/96-integration-tests-in-sdk
 git pull
 
@@ -24,4 +24,4 @@ docker image build --target nodejs . -f ./blockchain/Dockerfile --force-rm -t to
 
 # Run integration tests
 cd ../
-docker run -v ${PARENT_PATH}/..:/var/sdk tonomytestcontainer ./var/sdk/integration-test/run-tests-script.sh
+docker run -v ${PARENT_PATH}/..:/var/sdk tonomytestcontainer /var/sdk/integration-test/run-tests-script.sh
--- integration-test/run-tests-script.sh
+++ integration-test/run-tests-script.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-./nodeos.sh &
+echo "Running nodeos in background"
+./nodeos.sh > nodeos.log 2>&1 &
 
+echo "Waiting for nodeos to start"
 sleep 10
 
+echo "Running tests"
 cd /var/sdk
-npm run test
+npm run test:integration
