2f4dc4bfae76aa42cfbfbb15d731b0fd0cea374e
Jack Tanner <jack@jackandtheblockstalk.com>
2023-02-06 09:55:52 +0100

    Runs integration tests in host machine

--- integration-test/build-and-run-tests.sh
+++ integration-test/build-and-run-tests.sh
@@ -5,7 +5,8 @@ set -e ## exit if any statement fails
 
 # Make sure working dir is same as this dir, so that script can be excuted from another working directory
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-cd "${PARENT_PATH}/.."
+SDK_DIR="${PARENT_PATH}/.."
+cd "${SDK_DIR}"
 
 # Setup Tonomy Contracts
 if [ ! -d "Tonomy-Contracts" ]; then
@@ -20,8 +21,17 @@ git pull
 ./build-contracts.sh
 
 # Create docker image
-docker image build --target nodejs . -f ./blockchain/Dockerfile --force-rm -t tonomytestcontainer
+docker image build --target initialized . -f ./blockchain/Dockerfile --force-rm -t tonomytestimage
+
+# Run container
+cd "${SDK_DIR}"
+# docker run -v ${PARENT_PATH}/..:/var/sdk --name tonomytestcontainer tonomytestimage /var/sdk/integration-test/run-tests-script.sh
+docker run --name tonomytestcontainer -d tonomytestimage
+sleep 10
 
 # Run integration tests
-cd ../
-docker run -v ${PARENT_PATH}/..:/var/sdk tonomytestcontainer /var/sdk/integration-test/run-tests-script.sh
+set +e
+npm run test:integration
+
+docker exec -it tonomytestcontainer ./nodeos.sh stop
+set -e
