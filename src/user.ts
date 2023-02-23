b394e99541f5cbeaf2ceddd7a09559a54241301b
Jack Tanner <jack@jackandtheblockstalk.com>
2023-02-23 17:56:20 +0100

    fixed account status

--- src/user.ts
+++ src/user.ts
@@ -13,8 +13,8 @@ import { getSettings } from './settings';
 import { Communication } from './communication';
 
 enum UserStatus {
-    ACCOUNT_CREATED = 'CREATING',
-    LOGIN_IN = 'LOGGING_IN',
+    CREATING_ACCOUNT = 'CREATING_ACCOUNT',
+    LOGGING_IN = 'LOGGING_IN',
     READY = 'READY',
     DEACTIVATED = 'DEACTIVATED',
 }
@@ -271,7 +271,7 @@ export class User {
 
         this.storage.accountName = Name.from(idData.account_name);
         this.storage.username = username;
-        this.storage.status = UserStatus.LOGIN_IN;
+        this.storage.status = UserStatus.LOGGING_IN;
 
         await this.storage.accountName;
         await this.storage.username;
