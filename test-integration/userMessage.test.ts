dc58bbcf4a99f9d719775e218ba81f62d091e9bf
Jack Tanner <jack@jackandtheblockstalk.com>
2023-03-06 15:52:20 +0100

    turned on user message tests

--- test-integration/userMessage.test.ts
+++ test-integration/userMessage.test.ts
@@ -1,55 +1,43 @@
-// this needs to get uncommented when we have tonomy communication in the github action and integration tests
 import { setSettings } from '../src';
-// import { Message } from '../src/util/message';
+import { Message } from '../src/util/message';
 import settings from './services/settings';
-// import { createRandomID } from './util/user';
-
 import { createRandomID } from './util/user';
 
 setSettings(settings);
 
-// describe('user signing messages', () => {
-//     test('user can sign a message', async () => {
-//         expect.assertions(3);
-//         const { user } = await createRandomID();
-//         const payload = {
-//             id: 123,
-//             message: 'hi',
-//         };
-
-//         expect(user.signMessage(payload)).resolves.toBeTruthy();
-
-//         const message = await user.signMessage(payload);
-
-//         expect(message).toBeInstanceOf(Message);
-//         expect(message.getPayload()).toEqual(payload);
-//         user.logout();
-//     });
-
-//     test('user can verifies created message', async () => {
-//         expect.assertions(3);
-//         const { user } = await createRandomID();
-//         const payload = {
-//             id: 123444,
-//             message: 'h2',
-//         };
-
-//         const message = await user.signMessage(payload);
-
-//         expect(message).toBeTruthy();
-//         const verify = await message.verify();
-
-//         expect(verify).toBe(true);
-//         expect(message.getPayload()).toEqual(payload);
-//         user.logout();
-//     });
-// });
-
-describe('one test for the file', () => {
-    it('excpect user to be truthy', async () => {
+describe('user signing messages', () => {
+    test('user can sign a message', async () => {
+        expect.assertions(3);
         const { user } = await createRandomID();
+        const payload = {
+            id: 123,
+            message: 'hi',
+        };
 
-        expect(user).toBeTruthy();
-        await user.logout();
+        expect(user.signMessage(payload)).resolves.toBeTruthy();
+
+        const message = await user.signMessage(payload);
+
+        expect(message).toBeInstanceOf(Message);
+        expect(message.getPayload()).toEqual(payload);
+        user.logout();
+    });
+
+    test('user can verifies created message', async () => {
+        expect.assertions(3);
+        const { user } = await createRandomID();
+        const payload = {
+            id: 123444,
+            message: 'h2',
+        };
+
+        const message = await user.signMessage(payload);
+
+        expect(message).toBeTruthy();
+        const verify = await message.verify();
+
+        expect(verify).toBe(true);
+        expect(message.getPayload()).toEqual(payload);
+        user.logout();
     });
 });
