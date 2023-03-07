8c64301bcd00fe2c7f944dba0a5f7207f78cc6ac
Jack Tanner <jack@jackandtheblockstalk.com>
2023-03-07 14:14:34 +0100

    first part of external user test

---
+++ test-integration/externalUser.test.ts
@@ -0,0 +1,60 @@
+// need to use API types from inside tonomy-id-sdk, otherwise type compatibility issues
+import { createRandomApp, createRandomID } from './util/user';
+import { setSettings, User, AppStatus, Message, UserApps, JWTLoginPayload, App } from '../src/index';
+import settings from './services/settings';
+import { catchAndPrintErrors } from './util/errors';
+import { PublicKey } from '@greymass/eosio';
+
+setSettings(settings);
+
+describe('External User class', () => {
+    test(
+        'loginWithTonomy(): Logs into new app',
+        catchAndPrintErrors(async () => {
+            // Create new Tonomy ID user
+            const { user, auth } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+
+            // Login to Tonomy Communication
+            const message = await user.signMessage({});
+
+            await user.communication.login(message);
+
+            // Setup a promise that resolves when the subscriber executes
+            const subscriberPromise = new Promise((resolve, reject) => {
+                user.communication.subscribeMessage(async (m) => {
+                    const message = new Message(m);
+
+                    const requests = message.getPayload().requests;
+
+                    const verifiedRequests = await UserApps.verifyRequests(requests);
+
+                    for (const jwt of verifiedRequests) {
+                        const payload = jwt.getPayload() as JWTLoginPayload;
+                        const app = await App.getApp(payload.origin);
+
+                        const accountName = await user.storage.accountName.toString();
+
+                        await user.apps.loginWithApp(app, PublicKey.from(payload?.publicKey));
+                        // TODO: cover case where checked === true and we also need to login to the SSO Login App
+
+                        const recieverDid = jwt.getSender();
+                        const message = await user.signMessage({ requests, accountName }, recieverDid);
+
+                        user.communication.sendMessage(message);
+                    }
+
+                    resolve(true);
+                });
+            });
+
+            // Create an app
+            const app = await createRandomApp();
+
+            await subscriberPromise;
+
+            // Close connections
+            await user.logout();
+        })
+    );
+});
