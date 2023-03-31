1f1f02d0ddc77d6a97e16e06d0ada634988158fa
Jack Tanner <jack@jackandtheblockstalk.com>
2023-03-31 14:14:42 +0200

    chore: fixed linting errors

--- test/services/username.test.ts
+++ test/services/username.test.ts
@@ -8,6 +8,7 @@ setSettings({
 describe('TonomyUsername', () => {
     it('creates a username correctly', () => {
         const username = TonomyUsername.fromUsername('jack', AccountType.PERSON, getSettings().accountSuffix);
+
         expect(username.username).toEqual('jack.person.test.tonomy.id');
         expect(username.usernameHash).toEqual('1599187240569a41cad6b931b911202926fee043811a42f60807dd9ba18dc399');
     });
