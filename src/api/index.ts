d0760e06123df7ae2a929666b4740d12eda135a9
Jack Tanner <jack@jackandtheblockstalk.com>
2023-04-04 22:47:45 +0200

    modifyTonomyButton updates css styl of button

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,12 +2,6 @@ import { ExternalUser } from './externalUser';
 import { setSettings } from '../sdk';
 import { modifyTonomyButton } from './tonomyButton';
 
-try {
-    modifyTonomyButton();
-} catch (error) {
-    console.error('error: ', error);
-}
-
 export const api = {
     ExternalUser,
     setSettings,
--- src/api/tonomyButton.ts
+++ src/api/tonomyButton.ts
@@ -1,11 +1,20 @@
 export function modifyTonomyButton() {
-    console.log('modifyTonomyButton() called');
-
     const tonomyButton = document.getElementsByClassName('tonomy-login-button');
 
-    console.log('tonomyButton: ', tonomyButton);
-    if (!tonomyButton || tonomyButton.length === 0) throw new Error('tonomyButton not found');
+    if (!tonomyButton || tonomyButton.length === 0) throw new Error('element with class tonomy-login-button not found');
     const button = tonomyButton[0] as HTMLButtonElement;
 
-    if (!button) throw new Error('button not found');
+    if (!button) throw new Error('Button not found');
+
+    button.style.backgroundColor = 'red';
+    button.style.color = "white"
+    button.style.backgroundColor = "#67D7ED";
+    button.style.borderRadius = "1.5rem";
+    button.style.padding = ".5rem 2.5rem";
+    button.style.borderColor = "transparent";
+    button.style.fontSize = "1rem";
+    button.style.boxShadow = "rgba(0, 0, 0, 0.288) 1px 1px 10px";
+    button.style.fontWeight = "500";
+    button.style.cursor = "pointer";
+    button.style.transition = "all .5s ease"
 }
