8d4f11b3f4e7b242f4ed4c9e88b5a9049e7b5d61
Jack Tanner <jack@jackandtheblockstalk.com>
2023-04-05 10:33:17 +0200

    Tonomy button.css

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,7 @@
 import { ExternalUser } from './externalUser';
 import { setSettings } from '../sdk';
-import { modifyTonomyButton } from './tonomyButton';
 
 export const api = {
     ExternalUser,
     setSettings,
-    modifyTonomyButton,
 };
