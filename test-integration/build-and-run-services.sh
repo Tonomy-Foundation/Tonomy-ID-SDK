7263848060afb9a8e9721978bdd9de386fc2ef14
Jack Tanner <jack@jackandtheblockstalk.com>
2023-04-10 11:27:23 +0200

    chore: build and run services with start and stop command

--- test-integration/build-and-run-services.sh
+++ test-integration/build-and-run-services.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ARG1=$1
+
 set -u ## exit if you try to use an uninitialised variable
 set -e ## exit if any statement fails
 
@@ -8,44 +10,86 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 SDK_DIR="${PARENT_PATH}/.."
 cd "${SDK_DIR}"
 
-# Setup Tonomy Contracts and Tonomy Communication
-if [ ! -d "Tonomy-Contracts" ]; then
-    git submodule update --init --recursive
-    git submodule foreach git checkout development
-    git submodule foreach git pull
+function setup {
+    # Setup Tonomy Contracts and Tonomy Communication git submodules
+    if [ ! -d "Tonomy-Contracts" ]; then
+        git submodule update --init --recursive
+        git submodule foreach git checkout development
+        git submodule foreach git pull
+    fi
+
+    # Build Tonomy Blockchain image
+    cd "${SDK_DIR}/Tonomy-Contracts"
+    ./blockchain/build-docker.sh
+
+    # Install dependencies SDK
+    cd "${SDK_DIR}"
+    if [ ! -d "node_modules" ]; then
+        npm i
+    fi
+    if [ ! -d "build" ]; then
+        npm run build
+    fi
+    if [ ! -d "build/cli" ]; then
+        npm run build:cli
+    fi
+
+    # Install dependencies Tonomy Communication
+    cd "${SDK_DIR}/Tonomy-Communication"
+    if [ ! -d "node_modules" ]; then
+        npm i
+    fi
+}
+
+function start {
+    # Run blockchain node
+    cd "${SDK_DIR}"
+    docker run -p 8888:8888 --name tonomy_blockchain_integration -d tonomy_blockchain_initialized
+    echo "Waiting 8 seconds for blockchain node to start"
+    sleep 8
+
+    # Run Communication server
+    cd  "$SDK_DIR/Tonomy-Communication"
+    pm2 stop micro || true
+    pm2 delete micro || true
+    pm2 start yarn --name "micro" -- run start:dev
+}
+
+function bootstrap {
+    # Run bootstrap script
+    cd  "$SDK_DIR"
+    npm run cli bootstrap
+}
+
+function stop {
+    # Stop blockchain node
+    docker exec -it tonomy_blockchain_integration ./nodeos.sh stop || true
+    docker rm -f tonomy_blockchain_integration || true
+
+    # Stop Communication server
+    pm2 stop micro || true
+    pm2 delete micro || true
+}
+
+function help {
+    echo "Usage: $0 [start|stop]"
+    echo ""
+    echo "start: Setup and start blockchain node and communication server"
+    echo "stop: Stop blockchain node and communication server"
+}
+
+if [ -z "$ARG1" ]
+then
+    help
+elif [ "$ARG1" == "start" ]
+then
+    setup
+    stop
+    start
+    bootstrap
+elif [ "$ARG1" == "stop" ]
+then
+    stop
+else
+    help
 fi
-
-# Build Tonomy Blockchain image
-cd "${SDK_DIR}/Tonomy-Contracts"
-./blockchain/build-docker.sh
-
-# Install dependencies
-cd "${SDK_DIR}"
-if [ ! -d "node_modules" ]; then
-    npm i
-fi
-if [ ! -d "build" ]; then
-    npm run build
-fi
-cd "${SDK_DIR}/Tonomy-Communication"
-if [ ! -d "node_modules" ]; then
-    npm i
-fi
-
-# Run blockchain node
-cd "${SDK_DIR}"
-docker exec -it tonomy_blockchain_integration ./nodeos.sh stop || true
-docker rm -f tonomy_blockchain_integration || true
-docker run -p 8888:8888 --name tonomy_blockchain_integration -d tonomy_blockchain_initialized
-echo "Waiting 8 seconds for blockchain node to start"
-sleep 8
-
-# Run Communication server
-cd  "$SDK_DIR/Tonomy-Communication"
-pm2 stop micro || true
-pm2 delete micro || true
-pm2 start yarn --name "micro" -- run start:dev
-
-# Run bootstrap script
-cd  "$SDK_DIR"
-npm run cli bootstrap
