1b5701a80633e0e8334e65eaa9dc06b8522a9f0a
Jack Tanner <jack@jackandtheblockstalk.com>
2023-04-18 10:38:26 +0200

    test: tests for runtime test to check the generatePrivateKeyFromPassword() and argon2

--- test/util/crypto.test.ts
+++ test/util/crypto.test.ts
@@ -1,4 +1,7 @@
+import { Checksum256, PrivateKey } from '@greymass/eosio';
+import { generatePrivateKeyFromPassword } from '../../src/cli/bootstrap/keys';
 import { sha256, randomString, generateRandomKeyPair, randomBytes } from '../../src/sdk/util/crypto';
+import * as argon2 from 'argon2';
 
 describe('crypto sha256()', () => {
     it('sha256 hash', () => {
@@ -19,6 +22,52 @@ describe('crypto generateRandomKeyPair()', () => {
 
         expect(key1.privateKey.toString()).not.toEqual(key2.privateKey.toString());
         expect(key1.publicKey.toString()).not.toEqual(key2.publicKey.toString());
+    });
+});
+
+describe('crypto generatePrivateKeyFromPassword()', () => {
+    it('can generate a private key from a password', async () => {
+        const { privateKey, salt } = await generatePrivateKeyFromPassword('test');
+
+        expect(privateKey).toBeInstanceOf(PrivateKey);
+        expect(salt).toBeInstanceOf(Checksum256);
+    });
+
+    it('generates same private key from same salt', async () => {
+        const salt = randomBytes(32);
+        const hash = await generatePrivateKeyFromPassword('test', Checksum256.from(salt));
+        const hash2 = await generatePrivateKeyFromPassword('test', Checksum256.from(salt));
+
+        expect(hash2).toEqual(hash);
+    });
+
+    it('generatePrivateKeyFromPassword() creates the same private key from a password and salt as what happens in Tonomy ID', async () => {
+        const saltInput = Checksum256.from(sha256('testsalt'));
+
+        const { privateKey, salt } = await generatePrivateKeyFromPassword('testpassword', saltInput);
+
+        expect(salt.toString()).toBe('4edf07edc95b2fdcbcaf2378fd12d8ac212c2aa6e326c59c3e629be3039d6432');
+        expect(privateKey.toString()).toEqual('PVT_K1_NXkZkJyhrPzSCpfe2uXbaw8xcKH95e9Gw5LdhkrzMSJwoZL6x');
+    });
+
+    it('argon2 generates the same value as with https://argon2.online', async () => {
+        const password = 'testpassword';
+        const saltInput = Checksum256.from(sha256('testsalt'));
+        // 4edf07edc95b2fdcbcaf2378fd12d8ac212c2aa6e326c59c3e629be3039d6432
+
+        const hash = await argon2.hash(password, {
+            salt: Buffer.from(saltInput.hexString, 'hex'),
+            hashLength: 32,
+            type: argon2.argon2id,
+            raw: true,
+            memoryCost: 16384,
+            parallelism: 1,
+            timeCost: 1, // iterations
+        });
+
+        const bytes = new Uint8Array(hash);
+
+        expect(hash.toString('hex')).toEqual('30e30e19f23a98bdb2e932d8c0e40ca4471cc02bb39cc4b508afe30921b44573');
     });
 });
 
