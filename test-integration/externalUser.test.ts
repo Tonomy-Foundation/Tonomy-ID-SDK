0e72057ee220204b194286c21e450965a90b9288
Jack Tanner <jack@jackandtheblockstalk.com>
2023-04-19 15:53:36 +0200

    test: fixed number of expected checks in test

--- test-integration/externalUser.test.ts
+++ test-integration/externalUser.test.ts
@@ -100,7 +100,7 @@ describe('External User class', () => {
 
     describe('SSO login full end-to-end flow', () => {
         test('User succeeds at login to external website', async () => {
-            expect.assertions(32);
+            expect.assertions(34);
 
             const appsFound = [false, false];
 
