1d137f878ee67942d3561d3fa20d4fe6232e1918
sadiabbasi <sadiaabbas326@gmail.com>
2023-04-20 15:00:21 +0500

    update external user

--- test-integration/externalUser.test.ts
+++ test-integration/externalUser.test.ts
@@ -90,7 +90,7 @@ describe('External User class', () => {
 
     describe('SSO login full end-to-end flow', () => {
         test('User succeeds at login to external website', async () => {
-            expect.assertions(35);
+            expect.assertions(33);
 
             const appsFound = [false, false];
 
