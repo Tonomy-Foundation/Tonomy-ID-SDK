7e9323819166d7e8327b6d98ebd26cade3fc1fb5
sadiabbasi <sadiaabbas326@gmail.com>
2023-06-09 14:16:08 +0500

    feat: :fire: added tag in package.json

--- custom-tag-plugin.js
+++ custom-tag-plugin.js
@@ -1,16 +1,16 @@
 import { execSync } from 'child_process';
 
-export const prepare = (pluginConfig, context) => {
-    const { branch, nextRelease } = context;
-    let tagFormat;
+export default async function customSemanticReleaseScript(pluginConfig, context) {
+    const { branch } = context;
+    const tagPrefix = 'v';
+    const tagSuffix = '-devtest1';
 
-    if (branch.name === 'feature/232-sdk-deployment') {
-        tagFormat = `development-${nextRelease.version}`;
-    } else {
-        tagFormat = `v${nextRelease.version}`;
-    }
+    // Generate the tag name based on the branch name and desired convention
+    const tagName = `${tagPrefix}${branch.replace('/', '-')}${tagSuffix}`;
 
-    execSync(`git tag -a ${tagFormat} -m "chore(release): ${tagFormat}"`);
+    // Add the tag
+    execSync(`git tag ${tagName}`);
 
-    nextRelease.gitTag = tagFormat;
-};
+    // Push the tag
+    execSync(`git push origin ${tagName}`);
+}
