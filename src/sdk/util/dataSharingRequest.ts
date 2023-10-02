44678c86cea34e3bc2dc655824d38557fa36616d
sadia <sadia@Ubuntu20-dev.myguest.virtualbox.org>
2023-10-02 16:09:54 +0300

    fix the comments

--- src/sdk/util/dataSharingRequest.ts
+++ src/sdk/util/dataSharingRequest.ts
@@ -1,29 +1,17 @@
-import { PublicKey } from '@wharfkit/antelope';
-import { VCWithTypeType, VerifiableCredentialOptions, VerifiableCredentialWithType } from './ssi/vc';
+import { VerifiableCredentialOptions, VerifiableCredentialWithType } from './ssi/vc';
 import { Issuer } from '@tonomy/did-jwt-vc';
 
 export type DataSharingRequestPayload = {
-    username: string;
-    origin: string;
-    publicKey: PublicKey;
-    callbackPath: string;
+    username: boolean;
 };
 
 export class DataSharingRequest extends VerifiableCredentialWithType<DataSharingRequestPayload> {
     protected static type = 'DataSharingRequest';
 
     /**
-     * @override the VerifiableCredentialWithType constructor to decode the payload of type DataSharingRequestPayload
-     */
-    constructor(vc: DataSharingRequest | VCWithTypeType<DataSharingRequestPayload>) {
-        super(vc);
-        this.decodedPayload.publicKey = PublicKey.from(super.getPayload().publicKey);
-    }
-
-    /**
      * Alternative constructor that returns type DataSharingRequest
      */
-    static async createRequest(
+    static async signRequest(
         payload: DataSharingRequestPayload,
         issuer: Issuer,
         options: VerifiableCredentialOptions = {}
