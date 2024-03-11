28804c4d8ef7ebfb90e55b34e71807277363adf2
Jack Tanner <jack@jackandtheblockstalk.com>
2024-03-11 17:09:26 +0100

    fix: Fixed max inline action size

--- src/sdk/services/blockchain/contracts/EosioContract.ts
+++ src/sdk/services/blockchain/contracts/EosioContract.ts
@@ -234,7 +234,7 @@ export interface BlockchainParams {
 export const defaultBlockchainParams: BlockchainParams = {
     // NET
     max_block_net_usage: 1024 * 1024,
-    target_block_net_usage_pct: 8000,
+    target_block_net_usage_pct: 8000, // 80%
     max_transaction_net_usage: 1024 * 512,
     base_per_transaction_net_usage: 12,
     net_usage_leeway: 500,
@@ -242,14 +242,14 @@ export const defaultBlockchainParams: BlockchainParams = {
     context_free_discount_net_usage_den: 100,
     // CPU
     max_block_cpu_usage: 200000,
-    target_block_cpu_usage_pct: 8000,
+    target_block_cpu_usage_pct: 8000, // 80%
     max_transaction_cpu_usage: 50000,
     min_transaction_cpu_usage: 100,
     // Other
     max_transaction_lifetime: 3600,
     deferred_trx_expiration_window: 600,
     max_transaction_delay: 3888000,
-    max_inline_action_size: 4096,
+    max_inline_action_size: 32 * 1024,
     max_inline_action_depth: 4,
     max_authority_depth: 6,
 };
