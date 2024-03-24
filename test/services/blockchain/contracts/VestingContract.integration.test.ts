a2856ac0a184681a1bfc2f7d17a06d5640642a62
sadiabbasi <sadiaabbas326@gmail.com>
2024-03-25 00:26:04 +0300

    Working on integration test

--- test/services/blockchain/contracts/VestingContract.integration.test.ts
+++ test/services/blockchain/contracts/VestingContract.integration.test.ts
@@ -1,8 +1,8 @@
 import { VestngContract } from '../../../../src/sdk/index';
 import { setTestSettings } from '../../../helpers/settings';
 import { getSigner } from '../../../../src/cli/bootstrap/keys';
+import { createRandomID } from '../../../helpers/user';
 import { getAccountInfo } from '../../../../src/sdk/helpers/user';
-import { Name } from '@wharfkit/antelope';
 
 setTestSettings();
 
@@ -11,105 +11,240 @@ const vestngContract = VestngContract.Instance;
 describe('VestngContract class', () => {
     const signer = getSigner();
 
-    beforeEach(() => {
-        jest.setTimeout(60000);
-    });
-
-    test('setStartDate(): Successfully set start date', async () => {
-        expect.assertions(1);
-        const salesDate = '2023-03-20T00:00:00';
-        const launchDate = '2022-01-25T00:00:00';
-
+    test('assignTokens(): UnSuccessful assignment if date not set', async () => {
         try {
-            const trx = await vestngContract.updatedate(salesDate, launchDate, signer);
+            const salesDate = '';
+            const launchDate = '';
 
-            expect(trx.processed.receipt.status).toBe('executed');
+            await vestngContract.updatedate(salesDate, launchDate, signer);
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
+
+            await vestngContract.assignTokens('coinsale.tmy', accountName, '100.000000 LEOS', 1, signer);
         } catch (e) {
-            console.log(e.message, JSON.stringify(e, null, 2));
-            throw e;
+            expect(e).toBeDefined();
         }
     });
+
+    test('withdraw(): UnSuccessful withdraw if date not set', async () => {
+        try {
+            const salesDate = new Date().toISOString();
+            const launchDate = '';
+
+            await vestngContract.updatedate(salesDate, launchDate, signer);
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
+
+            await vestngContract.assignTokens('coinsale.tmy', accountName, '100.000000 LEOS', 1, signer);
+            await vestngContract.withdraw(accountName, signer);
+        } catch (e) {
+            expect(e).toBeDefined();
+        }
+    });
+
+    test('updatedate(): Successfully set start date', async () => {
+        expect.assertions(1);
+        const salesDate = new Date().toISOString();
+        const launchDate = new Date(Date.now() + 5000).toISOString();
+
+        const trx = await vestngContract.updatedate(salesDate, launchDate, signer);
+
+        expect(trx.processed.receipt.status).toBe('executed');
+    });
+
+    test('updatedate(): Unsuccessful if  date not format incorrect', async () => {
+        const salesDate = 'undefined';
+        const launchDate = 'undefined';
+
+        try {
+            await vestngContract.updatedate(salesDate, launchDate, signer);
+        } catch (e) {
+            expect(e.error.details[0].message).toContain('date parsing failed');
+        }
+    }, 10000);
 
     test('assignTokens(): Successfully assign tokens to a holder', async () => {
         expect.assertions(1);
 
-        try {
-            const trx = await vestngContract.assignTokens('eosio.token', 'ecosystm.tmy', '5000.000000 LEOS', 1, signer);
+        const { user } = await createRandomID();
+        const userAccountName = await user.storage.accountName;
+        const accountInfo = await getAccountInfo(userAccountName);
+        const accountName = accountInfo.account_name.toString();
 
-            expect(trx.processed.receipt.status).toBe('executed');
-        } catch (e) {
-            console.log(e.message, JSON.stringify(e, null, 2));
-            throw e;
-        }
+        const trx = await vestngContract.assignTokens('coinsale.tmy', accountName, '100.000000 LEOS', 1, signer);
+
+        expect(trx.processed.receipt.status).toBe('executed');
     });
 
     test('assignTokens(): Unsuccessful assignment due to invalid symbol', async () => {
         expect.assertions(1);
 
         try {
-            await vestngContract.assignTokens('eosio.token', 'ops.tmy', '10.000000 EOS', 1, signer);
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
+
+            await vestngContract.assignTokens('coinsale.tmy', accountName, '10.000000 EOS', 1, signer);
         } catch (e) {
             expect(e.error.details[0].message).toContain(' Symbol does not match system resource currency');
         }
     });
+
     test('assignTokens(): Unsuccessful assignment due to invalid amount', async () => {
         expect.assertions(1);
 
         try {
-            await vestngContract.assignTokens('eosio.token', 'ops.tmy', '-10.000000 LEOS', 1, signer);
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
+
+            await vestngContract.assignTokens('coinsale.tmy', accountName, '-10.000000 LEOS', 1, signer);
         } catch (e) {
             expect(e.error.details[0].message).toContain('Amount must be greater than 0');
         }
     });
-    test('withdraw(): Successful withdrawal with cliff period', async () => {
+
+    test('assignTokens(): Unsuccessful assignment due to sales not started', async () => {
         expect.assertions(1);
-        const holder = 'ecosystm.tmy';
+        const salesDate = new Date(Date.now() + 5000).toISOString();
+        const launchDate = new Date(Date.now() + 10000).toISOString();
 
         try {
-            const account2 = await getAccountInfo(Name.from('vestng.token'));
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
 
-            console.log('Account', JSON.stringify(account2.getPermission('active')));
-            const trx = await vestngContract.withdraw(holder, signer);
-
-            expect(trx.processed.receipt.status).toBe('executed');
+            await vestngContract.updatedate(salesDate, launchDate, signer);
+            await vestngContract.assignTokens('coinsale.tmy', accountName, '10.000000 LEOS', 1, signer);
         } catch (e) {
-            console.log(e.message, JSON.stringify(e, null, 2));
-            throw e;
+            expect(e.error.details[0].message).toContain('Sale has not yet started');
         }
     });
 
-    test('withdraw(): Successful withdrawal with 2 different allocations of same category', async () => {
+    test('assignTokens(): Unsuccessfull assignment due to invalid category', async () => {
         expect.assertions(1);
-        const holder = 'team.tmy';
 
         try {
-            await vestngContract.assignTokens('eosio.token', 'team.tmy', '3000.000000 LEOS', 1, signer);
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
 
-            await vestngContract.assignTokens('eosio.token', 'coinsale.tmy', '4000.000000 LEOS', 1, signer);
-
-            const trx1 = await vestngContract.withdraw(holder, signer);
-
-            expect(trx1.processed.receipt.status).toBe('executed');
+            await vestngContract.assignTokens('coinsale.tmy', accountName, '10.000000 LEOS', 0, signer);
         } catch (e) {
-            console.log(e.message, JSON.stringify(e, null, 2));
-            throw e;
+            expect(e.error.details[0].message).toContain('Invalid vesting category');
+        }
+    });
+
+    test('assignTokens(): Unsuccessful assignment due to number of purchases', async () => {
+        expect.assertions(1);
+
+        try {
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
+
+            const promises = [];
+
+            for (let i = 0; i < 21; i++) {
+                promises.push(
+                    new Promise((resolve, reject) => {
+                        setTimeout(() => {
+                            vestngContract
+                                .assignTokens('coinsale.tmy', accountName, '10.000000 LEOS', 1, signer)
+                                .then(resolve)
+                                .catch(reject);
+                        }, i * 2000); // Delay each transaction by 2 seconds
+                    })
+                );
+            }
+
+            await Promise.all(promises);
+        } catch (e) {
+            expect(e.message).toContain('Cannot purchase tokens more than 20 times.');
+        }
+    }, 60000);
+
+    test('withdraw(): Unsuccessful withdrawal vesting period after cliff not started', async () => {
+        expect.assertions(1);
+        const salesDate = '2023-03-27T00:00:00';
+        const launchDate = '2023-06-25T00:00:00';
+
+        try {
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
+
+            await vestngContract.updatedate(salesDate, launchDate, signer);
+
+            await vestngContract.assignTokens('coinsale.tmy', accountName, '100.000000 LEOS', 1, signer);
+
+            await vestngContract.withdraw(accountName, signer);
+        } catch (e) {
+            expect(e.error.details[0].message).toContain('Vesting period after cliff has not started');
+        }
+    });
+
+    test('withdraw(): Successful withdrawal with cliff period', async () => {
+        expect.assertions(1);
+        const salesDate = '2023-03-20T00:00:00';
+        const launchDate = '2022-01-25T00:00:00';
+
+        const { user } = await createRandomID();
+        const userAccountName = await user.storage.accountName;
+        const accountInfo = await getAccountInfo(userAccountName);
+        const accountName = accountInfo.account_name.toString();
+
+        await vestngContract.updatedate(salesDate, launchDate, signer);
+        await vestngContract.assignTokens('coinsale.tmy', accountName, '100.000000 LEOS', 1, signer);
+
+        const trx = await vestngContract.withdraw(accountName, signer);
+
+        expect(trx.processed.receipt.status).toBe('executed');
+    });
+
+    test('withdraw(): Successful withdrawal with 2 different allocations of same category', async () => {
+        expect.assertions(2);
+
+        const category = 1;
+
+        for (let i = 0; i < 2; i++) {
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
+
+            await vestngContract.assignTokens('eosio.token', accountName, '500.000000 LEOS', category, signer);
+            const trx = await vestngContract.withdraw(accountName, signer);
+
+            expect(trx.processed.receipt.status).toBe('executed');
         }
     }, 10000);
 
-    test('withdraw():  successful withdrawal with 2 different allocations of different category', async () => {
-        expect.assertions(111);
-        const holder = 'ops.tmy';
+    test('withdraw(): Successful withdrawal with 2 different allocations of different category', async () => {
+        expect.assertions(2);
 
-        try {
-            await vestngContract.assignTokens('eosio.token', 'ops.tmy', '1000.000000 LEOS', 1, signer);
-            await vestngContract.assignTokens('eosio.token', 'ecosystm.tmy', '1000.000000 LEOS', 2, signer);
+        const category = [1, 2];
 
-            const trx1 = await vestngContract.withdraw(holder, signer);
+        for (let i = 0; i < 2; i++) {
+            const { user } = await createRandomID();
+            const userAccountName = await user.storage.accountName;
+            const accountInfo = await getAccountInfo(userAccountName);
+            const accountName = accountInfo.account_name.toString();
 
-            expect(trx1.processed.receipt.status).toBe('executed');
-        } catch (e) {
-            console.log(e.message, JSON.stringify(e, null, 2));
-            throw e;
+            await vestngContract.assignTokens('eosio.token', accountName, '500.000000 LEOS', category[i], signer);
+            const trx = await vestngContract.withdraw(accountName, signer);
+
+            expect(trx.processed.receipt.status).toBe('executed');
         }
     }, 10000);
 });
