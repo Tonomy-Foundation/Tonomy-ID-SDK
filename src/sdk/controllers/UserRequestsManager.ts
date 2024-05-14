a141c426a3e6a61f1ea202c7421f68f10eb682b4
Jack Tanner <jack@jackandtheblockstalk.com>
2024-05-14 21:03:35 +0200

    docs: chain interface

--- src/sdk/controllers/UserRequestsManager.ts
+++ src/sdk/controllers/UserRequestsManager.ts
@@ -45,6 +45,9 @@ export class UserRequestsManager extends UserCommunication implements IUserReque
             // Throws SdkErrors.DataQueryNoRowDataFound error if app does not exist
             // which cannot happen in theory, as the user is already logged in
 
+            if (permission !== contract.toString())
+                throwError('Contract and permission do not match', SdkErrors.MismatchedParams);
+
             const signer = createKeyManagerSigner(this.keyManager, KeyManagerLevel.ACTIVE);
 
             await tonomyEosioProxyContract.linkAuth(
