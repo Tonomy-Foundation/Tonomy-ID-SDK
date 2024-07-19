840c9d4dc2823aca0acd7929b1d68b4f80172a99
Jack Tanner <jack@jackandtheblockstalk.com>
2024-07-19 19:26:32 +0200

    feat: removed unused base64 utils and tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import eslint from "@eslint/js";
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier/recommended';
-import globals from "globals";
+import globals from 'globals';
 
 export default [
     eslint.configs.recommended,
@@ -9,45 +9,43 @@ export default [
     prettier,
     {
         rules: {
-            eqeqeq: "error",
-            "no-console": "warn",
-            indent: ["error", 4, { SwitchCase: 1 }],
-            camelcase: "error",
-            "prettier/prettier": "error",
-            "@typescript-eslint/explicit-module-boundary-types": "off",
-            "@typescript-eslint/explicit-function-return-type": "off",
-            "@typescript-eslint/no-explicit-any": "warn",
-            "@typescript-eslint/no-unused-vars": "warn",
-            "padding-line-between-statements": [
-                "warn",
-                { blankLine: "always", prev: "block-like", next: "*" },
-                { blankLine: "always", prev: "block", next: "*" },
-                { blankLine: "always", prev: "*", next: ["block", "block-like"] },
-                { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
-                { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let", "var"] },
-                { blankLine: "always", prev: ["export", "import"], next: "*" },
-                { blankLine: "any", prev: "import", next: "import" },
-                { blankLine: "any", prev: "export", next: "export" }
-            ]
+            eqeqeq: 'error',
+            'no-console': 'warn',
+            indent: ['error', 4, { SwitchCase: 1 }],
+            camelcase: 'error',
+            'prettier/prettier': 'error',
+            '@typescript-eslint/explicit-module-boundary-types': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'padding-line-between-statements': [
+                'warn',
+                { blankLine: 'always', prev: 'block-like', next: '*' },
+                { blankLine: 'always', prev: 'block', next: '*' },
+                { blankLine: 'always', prev: '*', next: ['block', 'block-like'] },
+                { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+                { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+                { blankLine: 'always', prev: ['export', 'import'], next: '*' },
+                { blankLine: 'any', prev: 'import', next: 'import' },
+                { blankLine: 'any', prev: 'export', next: 'export' },
+            ],
         },
         languageOptions: {
             ecmaVersion: 6,
-            sourceType: "module",
+            sourceType: 'module',
             parserOptions: {
                 ecmaFeatures: {
-                    jsx: true
-                }
+                    jsx: true,
+                },
             },
             globals: {
                 ...globals.browser,
                 ...globals.node,
                 ...globals.es6,
-                ...globals.jest
-            }
+                ...globals.jest,
+            },
         },
-        files: ["**/*.js", "**/*.ts"],
-        ignores: [
-            ".build/**",
-        ]
-    }
+        files: ['**/*.js', '**/*.ts'],
+        ignores: ['build/**'],
+    },
 ];
