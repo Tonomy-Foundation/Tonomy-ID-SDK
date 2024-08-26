71f4edd8853a097b42f0340d235e734f95bd8408
sadiabbasi <sadiaabbas326@gmail.com>
2024-08-26 11:55:13 +0300

    Added create account

--- src/cli/accounts/index.ts
+++ src/cli/accounts/index.ts
@@ -1,11 +1,13 @@
-import { Name } from '@wharfkit/antelope';
-import { AccountType, SdkError, SdkErrors, TonomyContract, TonomyUsername, getAccountInfo } from '../../sdk';
+import { Name, PrivateKey } from '@wharfkit/antelope';
+import { AccountType, EosioContract, Authority, SdkError, SdkErrors, EosioUtil,  TonomyContract, TonomyUsername, getAccountInfo } from '../../sdk';
 import { setSettings } from '../../sdk/util/settings';
 import settings from '../bootstrap/settings';
+import { getDefaultAntelopePublicKey } from '../bootstrap/keys';
 
 setSettings(settings.config);
 
 const tonomyContract = TonomyContract.Instance;
+const eosioContract = EosioContract.Instance;
 
 export default async function apps(args: string[]) {
     if (args[0] === 'get') {
@@ -33,6 +35,25 @@ export default async function apps(args: string[]) {
             } else {
                 throw e;
             }
+        }
+    } else if (args[0] === 'create') {
+        console.log('Creating new account');
+        const account = args[1];
+        const accountKey = args[2] ?? getDefaultAntelopePublicKey().toString();
+      
+        const ownerAuth = Authority.fromKey(accountKey);
+
+        const activeAuth = Authority.fromKey(accountKey);
+        if(process.env.TONOMY_OPS_PRIVATE_KEY){
+            const newPrivateKey = PrivateKey.from(process.env.TONOMY_OPS_PRIVATE_KEY);
+            const newSigner = EosioUtil.createSigner(newPrivateKey);
+
+            // need to add the eosio.code authority as well so that it can call eosio from the smart contract
+            ownerAuth.addCodePermission(account);
+            activeAuth.addCodePermission(account);
+            await eosioContract.newaccount('eosio', account, ownerAuth, activeAuth, newSigner);
+        } else{
+            throw new Error(`TONOMY_OPS_PRIVATE_KEY not found`);
         }
     } else {
         throw new Error(`Unknown command ${args[0]}`);
