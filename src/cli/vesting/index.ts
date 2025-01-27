b50802747d3a4f6c20dfb1e302c923e6e4aada9a
Jack Tanner <jack@jackandtheblockstalk.com>
2025-01-27 22:23:55 +0100

    feat: audit CLI command

--- src/cli/vesting/index.ts
+++ src/cli/vesting/index.ts
@@ -49,6 +49,49 @@ export default async function vesting(args: string[]) {
         const res = await vestingContract.assignTokens(sender, recipient, quantity, categoryId, signer);
 
         console.log('Transaction ID: ', JSON.stringify(res, null, 2));
+    } else if (args[0] === 'audit') {
+        const action = 'assigntokens';
+        const contract = 'vesting.tmy';
+        const limit = 100;
+        let skip = 0;
+        let actionsFound = 0;
+
+        const uniqueHolders = new Set<string>();
+
+        do {
+            const url = `https://pangea.eosusa.io/v2/history/get_actions?act.name=${action}&sort=desc&skip=${skip}&limit=${limit}&account=${contract}&global_sequence=0-45539775`;
+            const res = await fetch(url);
+            const data = await res.json();
+            const actions = data.actions;
+
+            actionsFound = data?.actions?.length || 0;
+
+            for (const action of actions) {
+                const { sender, holder, amount, category } = action.act.data;
+
+                uniqueHolders.add(holder);
+
+                console.log(`${action.timestamp}: Sent ${amount} from ${sender} to ${holder} in category ${category}`);
+            }
+
+            skip += limit;
+        } while (actionsFound > 0);
+
+        console.log('');
+        console.log('Unique holders: ', uniqueHolders);
+        console.log('');
+
+        for (const holder of uniqueHolders) {
+            const allocations = await vestingContract.getAllocations(holder);
+
+            for (const allocation of allocations) {
+                // eslint-disable-next-line camelcase
+                const { id, tokens_allocated, vesting_category_type } = allocation;
+
+                // eslint-disable-next-line camelcase
+                console.log(`Holder ${holder}: ${id} - ${tokens_allocated} LEOS in category ${vesting_category_type}`);
+            }
+        }
     } else if (args[0] === 'setsettings') {
         const privateKey = PrivateKey.from(process.env.SIGNING_KEY || '');
         const signer = createSigner(privateKey);
