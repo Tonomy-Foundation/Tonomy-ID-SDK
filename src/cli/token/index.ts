e0b189c6472dc49e370a0f415b3e78ecc9f4a5b1
Jack Tanner <jack@jackandtheblockstalk.com>
2025-02-26 15:17:31 +0100

    feat: checking apps and people

--- src/cli/token/index.ts
+++ src/cli/token/index.ts
@@ -1,6 +1,6 @@
 import { Checksum256, Name, PrivateKey } from '@wharfkit/antelope';
 import { AccountType, TonomyUsername, EosioTokenContract } from '../../sdk';
-import { createSigner, getAccount, getAccountNameFromUsername } from '../../sdk/services/blockchain';
+import { assetToAmount, createSigner, getAccount, getAccountNameFromUsername } from '../../sdk/services/blockchain';
 import { getApi } from '../../sdk/services/blockchain/eosio/eosio';
 import settings from '../settings';
 import {
@@ -11,6 +11,7 @@ import {
     systemAccount,
 } from '../bootstrap';
 import Decimal from 'decimal.js';
+import { getAllAllocations, getAllUniqueHolders } from '../vesting';
 
 const tokenContract = EosioTokenContract.Instance;
 
@@ -58,14 +59,11 @@ interface AccountTokenData {
 }
 
 const ZERO_DECIMAL = new Decimal(0);
-const LEOS_SUPPLY = new Decimal(EosioTokenContract.TOTAL_SUPPLY);
-
-interface AppTokenData extends AccountTokenData {
-    ramQuota: number;
-    ramUsage: number;
-}
+const LEOS_SUPPLY_DECIMAL = new Decimal(EosioTokenContract.TOTAL_SUPPLY);
 
 export async function audit() {
+    console.log('');
+    console.log('Fetching tokens for bootstrap accounts');
     const bootstrappedAccounts = new Set<string>();
 
     bootstrappedAccounts.add(foundAccount);
@@ -88,58 +86,114 @@ export async function audit() {
         })
     );
 
-    bootstrappedData.forEach((data, key) => {
-        const fraction = data.tokens.div(LEOS_SUPPLY).mul(100).toFixed(8) + '%';
+    bootstrappedData.forEach((data, account) => {
+        const fraction = data.tokens.div(LEOS_SUPPLY_DECIMAL).mul(100).toFixed(8) + '%';
 
-        console.log(`${key.padEnd(14)} ${fraction.padStart(12)} (${data.tokens.toFixed(4).padStart(15)} LEOS)`);
+        console.log(`${account.padEnd(14)} ${data.tokens.toFixed(4).padStart(15)} LEOS (${fraction.padStart(12)})`);
     });
-    return;
 
-    // check how much total vested LEOS there is in the different categories and print. this shows us sales
-    const appAccounts = new Map<string, AppTokenData>();
+    console.log('');
+    console.log('Fetching vested tokens');
+    const vestingHolders = await getAllUniqueHolders();
+    const vestingAllocations = await getAllAllocations(vestingHolders);
+    const vestingCategories = vestingAllocations.reduce<number[]>((previous, allocation) => {
+        if (previous.includes(allocation.vesting_category_type)) return previous;
+        else return [...previous, allocation.vesting_category_type];
+    }, []);
+    const vestedTokensPerCategory = vestingCategories.reduce<Map<number, Decimal>>(
+        (map, category) => map.set(category, new Decimal(0)),
+        new Map<number, Decimal>()
+    );
+
+    for (const allocation of vestingAllocations) {
+        const categoryTokens = vestedTokensPerCategory.get(allocation.vesting_category_type);
+
+        if (!categoryTokens) throw new Error('categoryTokens undefined');
+        const allocationTokens = new Decimal(allocation.tokens_allocated.split(' ')[0]);
+
+        vestedTokensPerCategory.set(allocation.vesting_category_type, categoryTokens.add(allocationTokens));
+    }
+
+    const totalVested = vestingAllocations.reduce(
+        (previous, allocation) => (previous += assetToAmount(allocation.tokens_allocated)),
+        0
+    );
+
+    console.log('Total unique holders: ', vestingHolders.size);
+    console.log('Total vesting allocations: ', vestingAllocations.length);
+    console.log(
+        `Total vested:  ${totalVested.toFixed(4).padStart(15)} LEOS (${((100 * totalVested) / EosioTokenContract.TOTAL_SUPPLY).toFixed(8).padStart(11)}%)`
+    );
+    vestedTokensPerCategory.forEach((tokens, category) => {
+        const fraction = tokens.mul(100).dividedBy(EosioTokenContract.TOTAL_SUPPLY).toFixed(8) + '%';
+
+        console.log(
+            `> category ${category.toString().padStart(2)}: ${tokens.toFixed(4).padStart(15)} LEOS (${fraction.padStart(12)})`
+        );
+    });
+
+    console.log('');
+    console.log('Fetching apps tokens');
 
     const apps = await getAllApps();
 
-    await Promise.all(
+    const appAccounts = await Promise.all(
         apps.map(async (app) => {
             const balance = new Decimal(await tokenContract.getBalance(app.account_name));
             const account = await getAccount(app.account_name);
 
-            appAccounts.set(app.account_name.toString(), {
+            return {
+                account: app.account_name.toString(),
                 description: app.description,
                 tokens: balance,
                 vested: ZERO_DECIMAL,
                 ramQuota: account.ram_quota.toNumber(),
                 ramUsage: account.ram_usage.toNumber(),
-            });
+            };
         })
     );
 
-    console.log('App accounts: ', appAccounts);
+    const totalAllTokens = appAccounts.reduce((previous, app) => previous.add(app.tokens), ZERO_DECIMAL);
 
-    const peopleAccounts = new Map<string, AccountTokenData>();
+    console.log('Total apps', apps.length);
+    console.log(
+        `Total app tokens:  ${totalAllTokens.toFixed(4).padStart(14)} LEOS (${totalAllTokens.mul(100).dividedBy(EosioTokenContract.TOTAL_SUPPLY).toFixed(8).padStart(11)}%)`
+    );
+
+    console.log('');
+    console.log('Fetching people tokens');
     const people = await getAllPeople();
 
-    await Promise.all(
-        people.map(async (person) => {
-            const balance = new Decimal(await tokenContract.getBalance(person.account_name));
+    const peopleAccounts = [];
+    const batchSize = 100;
 
-            peopleAccounts.set(person.account_name, {
-                description: 'Person',
-                tokens: balance,
-                vested: ZERO_DECIMAL,
-            });
-        })
+    for (let i = 0; i < people.length; i += batchSize) {
+        const batch = people.slice(i, i + batchSize);
+
+        const batchResults = await Promise.all(
+            batch.map(async (person) => {
+                const balance = new Decimal(await tokenContract.getBalance(person.account_name));
+
+                return {
+                    account: person.account_name,
+                    description: person.status,
+                    tokens: balance,
+                    vested: ZERO_DECIMAL,
+                };
+            })
+        );
+
+        peopleAccounts.push(...batchResults);
+    }
+
+    const totalPeopleTokens = peopleAccounts.reduce((previous, person) => previous.add(person.tokens), ZERO_DECIMAL);
+
+    console.log('Total people', people.length);
+    console.log(
+        `Total people tokens:  ${totalPeopleTokens.toFixed(4).padStart(14)} LEOS (${totalPeopleTokens.mul(100).dividedBy(EosioTokenContract.TOTAL_SUPPLY).toFixed(8).padStart(11)}%)`
     );
 
-    console.log('People', peopleAccounts);
-
-    // get all bootstrap account, apps, people, advisors, block producers and team
-    // check allocations on all bootstrapped accounts
-    // check all allocations on all apps
-    // check all allocations on all people
-    // check all vested allocations in vesting contract
-    // check all staking allocations in staking contract
+    // TODO: check all staking allocations in staking contract
 }
 
 async function getAllApps() {
@@ -164,7 +218,7 @@ async function getAllApps() {
     });
 }
 
-async function getAllPeople() {
+async function getAllPeople(print = false) {
     const api = await getApi();
 
     const limit = 100;
@@ -174,7 +228,7 @@ async function getAllPeople() {
     const people: any[] = [];
 
     do {
-        console.log(`get_table_rows: people, ${limit}, ${lowerBound.toString()}`);
+        if (print) console.log(`get_table_rows: people, ${limit}, ${lowerBound.toString()}`);
         const data = await api.v1.chain.get_table_rows({
             code: 'tonomy',
             scope: 'tonomy',
