a594a15cfdec4eb93389bc39d60124df9cdf1c40
sadiabbasi <sadiaabbas326@gmail.com>
2025-03-27 15:02:00 +0300

    fix: :zap: fixed app.ts

--- src/sdk/controllers/App.ts
+++ src/sdk/controllers/App.ts
@@ -96,13 +96,14 @@ export class App implements AppData {
 
     static async getApp(origin: string): Promise<App> {
         const contractAppData = await tonomyContract.getApp(origin);
+        const appData = JSON.parse(contractAppData.json_data);
 
         return new App({
             accountName: contractAppData.account_name,
-            appName: contractAppData.app_name,
+            appName: appData.app_name,
             usernameHash: contractAppData.username_hash,
-            description: contractAppData.description,
-            logoUrl: contractAppData.logo_url,
+            description: appData.description,
+            logoUrl: appData.logo_url,
             origin: contractAppData.origin,
             version: contractAppData.version,
             status: AppStatusEnum.READY,
