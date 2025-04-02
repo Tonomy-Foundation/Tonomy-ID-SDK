08015b324afc3aaf6a7470564fb9afc6e0c7956b
Jack Tanner <jack@jackandtheblockstalk.com>
2025-04-02 18:59:09 +0200

    refactor: update token symbol

--- src/cli/msig/vestingMigrateAllocate.ts
+++ src/cli/msig/vestingMigrateAllocate.ts
@@ -131,7 +131,7 @@ export async function vestingMigrate3(options: StandardProposalOptions) {
 
         const actions: ActionData[] = batch.map((allocation) => {
             const newAmount = assetToDecimal(allocation.tokens_allocated).mul(priceMultiplier);
-            const newAsset = `${newAmount.toFixed(6)} LEOS`;
+            const newAsset = `${newAmount.toFixed(6)} TONO`;
 
             console.log(
                 `Migrating account ${allocation.holder} allocation ${allocation.id} in category ${allocation.vesting_category_type} from ${allocation.tokens_allocated} to ${newAsset}`
@@ -188,17 +188,17 @@ async function createAccountActions(account: NameType): Promise<ActionData[]> {
 
         if (oldCategory === 1 || oldCategory === 2) {
             if (oldCategory === 1) {
-                const oldLeosPrice = 0.002;
-                const newLeosPrice = TONO_SEED_ROUND_PRICE;
+                const oldTokenPrice = 0.002;
+                const newTokenPrice = TONO_SEED_ROUND_PRICE;
                 const amount = assetToAmount(allocation.tokens_allocated);
 
-                newAmount = (amount * oldLeosPrice) / newLeosPrice;
+                newAmount = (amount * oldTokenPrice) / newTokenPrice;
             } else {
-                const oldLeosPrice = 0.004;
-                const newLeosPrice = TONO_SEED_LATE_ROUND_PRICE;
+                const oldTokenPrice = 0.004;
+                const newTokenPrice = TONO_SEED_LATE_ROUND_PRICE;
                 const amount = assetToAmount(allocation.tokens_allocated);
 
-                newAmount = (amount * oldLeosPrice) / newLeosPrice;
+                newAmount = (amount * oldTokenPrice) / newTokenPrice;
 
                 newCategory = 9;
             }
