f033b26404d7dc3a2f1a8f793c5e56c6a9f84d05
Jack Tanner <jack@jackandtheblockstalk.com>
2025-05-20 23:56:43 +0200

    refactor: updated Contract.ts

--- src/sdk/services/blockchain/contracts/Contract.ts
+++ src/sdk/services/blockchain/contracts/Contract.ts
@@ -1,50 +1,33 @@
-import { NameType, PermissionLevel, PermissionLevelType, Action } from '@wharfkit/antelope';
+import { Name, NameType, PermissionLevel, PermissionLevelType, Action } from '@wharfkit/antelope';
 import { ContractKit, Contract as AntelopeContract } from '@wharfkit/contract';
 import { getApi } from '../eosio/eosio';
 
-function activeAuthorization(account: NameType): PermissionLevelType {
+export function activeAuthorization(account: NameType): PermissionLevelType {
     return PermissionLevel.from({ actor: account, permission: 'active' });
 }
 
+const kitPromise = getApi().then((api) => new ContractKit({ client: api }));
+
 export class Contract {
-    contractName?: NameType;
-    protected contract?: AntelopeContract;
+    contractName: Name;
+    contract: AntelopeContract;
 
-    constructor(contractName?: NameType) {
-        this.contractName = contractName;
+    constructor(contract: AntelopeContract) {
+        this.contract = contract;
+        this.contractName = contract.account;
     }
 
-    static async atContract<T extends Contract>(this: new (name: NameType) => T, account: NameType): Promise<T> {
-        const instance = new this(account);
+    static async atContract<Contract>(account: NameType): Promise<Contract> {
+        const contract = await (await kitPromise).load(account);
 
-        await instance.load();
-        return instance;
-    }
-
-    protected async load(): Promise<void> {
-        if (!this.contract) {
-            const api = await getApi();
-            const kit = new ContractKit({ client: api });
-
-            if (!this.contractName) throw new Error('Contract name not set');
-            this.contract = await kit.load(this.contractName);
-        }
-    }
-
-    protected async getContract(): Promise<AntelopeContract> {
-        await this.load();
-        return this.contract as AntelopeContract;
+        return new this(contract);
     }
 
     protected async action(
-        name: string,
-        data: any,
+        name: NameType,
+        data: object,
         authorization: PermissionLevelType[] = [activeAuthorization(this.contractName as NameType)]
     ): Promise<Action> {
-        const contract = await this.getContract();
-
-        return contract.action(name, data, authorization);
+        return this.contract.action(name.toString(), data, authorization);
     }
 }
-
-export { activeAuthorization };
