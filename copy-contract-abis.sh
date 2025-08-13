241304af6cc001f30a1fea807c1a693a00511f6b
Jack Tanner <jack@jackandtheblockstalk.com>
2025-08-13 17:00:41 +0200

    build: removed ABIs from source

--- copy-contract-abis.sh
+++ copy-contract-abis.sh
@@ -9,7 +9,13 @@ set -e ## exit if any statement fails
 # Make sure working dir is same as this dir, so that script can be excuted from another working directory
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-echo "Copying smart contracts ABIs"
+echo "Checking for submodules"
+git submodule update --init --recursive
+
+echo "Tonomy Contracts: Building smart contracts"
+cd "${PARENT_PATH}/Tonomy-Contracts"
+./build-contracts.sh
+
 CONTRACTS=(
     "eosio.bios"
     "eosio.msig"
@@ -22,22 +28,22 @@ CONTRACTS=(
 
 for CONTRACT in "${CONTRACTS[@]}"
 do
-    echo "Copying ${CONTRACT} ABI"
-    cp "${PARENT_PATH}/Tonomy-Contracts/contracts/${CONTRACT}/${CONTRACT}.abi" "${PARENT_PATH}/src/sdk/services/blockchain/contracts/abi/${CONTRACT}.abi.json"
+    echo "Tonomy Contracts: Copying ${CONTRACT} ABI"
+    cp "./contracts/${CONTRACT}/${CONTRACT}.abi" "${PARENT_PATH}/src/sdk/services/blockchain/contracts/abi/${CONTRACT}.abi.json"
 done
-
-echo "All contract ABIs copied successfully"
 
 # Build Ethereum Token
 cd "${PARENT_PATH}/Ethereum-token"
 if [ ! -d "node_modules" ]; then
-    echo "EthereumToken: Installing dependencies"
+    echo "Ethereum Token Contract: Installing dependencies"
     yarn install
 fi
 if [ ! -d "artifacts" ]; then
-    echo "EthereumToken: Compiling contracts"
+    echo "Ethereum Token Contract: Compiling contracts"
     yarn compile
 fi
 
-echo "Copying EthereumToken ABI"
+echo "Ethereum Token Contract: Copying ABI"
 cp "${PARENT_PATH}/Ethereum-token/artifacts/contracts/TonomyToken.sol/TonomyToken.json" "${PARENT_PATH}/src/sdk/services/ethereum/abi/TonomyToken.json"
+
+echo "All contract ABIs copied successfully"
