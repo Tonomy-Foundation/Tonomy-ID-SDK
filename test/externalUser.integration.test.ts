e37a0d595006ecd34d53652cfa8a56e0283b844b
sadiabbasi <sadiaabbas326@gmail.com>
2025-09-15 14:57:29 +0300

    fix: :zap: update base address

--- test/externalUser.integration.test.ts
+++ test/externalUser.integration.test.ts
@@ -137,7 +137,7 @@ describe('Login to external website', () => {
         );
 
         tonomyLoginApp = await createRandomApp();
-        baseAddress = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266';
+        baseAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
         destination = 'hardhat';
        
         setSettings({
