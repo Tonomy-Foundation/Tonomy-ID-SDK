49ac38b2801da0d94555998a792a6e7c130c1195
sadiabbasi <sadiaabbas326@gmail.com>
2025-09-25 22:21:32 +0300

    fix: :zap: revert developer console app

--- src/cli/bootstrap/index.ts
+++ src/cli/bootstrap/index.ts
@@ -438,6 +438,18 @@ async function createTonomyApps(newPublicKey: PublicKey, newSigner: Signer): Pro
     });
 
     await createApp({
+        appName: 'Developers Console',
+        usernamePrefix: 'developer-console',
+        description: `Developer console to manage ${settings.config.ecosystemName} applications and infrastucture`,
+        origin: settings.config.consoleWebsiteOrigin,
+        logoUrl: settings.config.consoleWebsiteOrigin + '/tonomy-logo1024.png',
+        backgroundColor: '#251950',
+        accentColor: '#BA54D3',
+        publicKey: newPublicKey,
+        signer,
+    });
+
+    await createApp({
         appName: 'Tonomy',
         usernamePrefix: 'tonomy-apps',
         description: `Access all your Tonomy apps in one hub. Manage tokens, explore the blockchain, create, collaborate, and build — it’s all at your fingertips`,
